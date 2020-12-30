97b5c109342aab7d57dbb10b0048762dfea600ea
akivalencia <adilene.valencias@gmail.com>
2020-12-29 21:31:45 -0800

    updating exercise

---
+++ datatypes.js
@@ -0,0 +1,138 @@
+// Week 1 - Variables
+
+// In the following exercises, you will need to place your code or answer underneath each
+// exercise prompt.
+
+// First try answering these without using references or looking up any information.
+// Then, check your answer by using references and/or running your code.
+// You can run your JS code using the Chrome or Firefox Developer tools, or by using Node.js.
+// Feel free to update your answers if you got them wrong at first -- this exercise is for your own learning.
+// But make sure you understand why the correct answer is right.
+
+// Data Types and Variables
+
+// Exercise 1. Simply declare a variable named 'emptyVariable'
+
+var emptyVariable = '';
+
+// Exercise 2. Declare a variable called 'myName'
+var myName = '';
+// Exercise 3. Assign your name as the value for 'myName'
+myName = 'Adi';
+// Exercise 4. Declare a variable called dreamDestination and give it the value
+// of what your dream vacation destination is.
+const dreamDestination = 'Puerto Vallarta';
+
+// Exercise 5. Make the following variables and assignments
+
+/*1 < var num1 < 10;
+1 <= var num2 <= 10;
+why the isn't this working omg :( EHHH AHHHHHHHHHHHHHH )
+*/
+
+// - a variable called num1 with a value greater than 1 and less than 10
+// - a variable called num2 with a value of greater than or equal to 1 and less
+// than or equal to 10
+
+// Exercise 6.  for num1 and num2 from Exercise 5. What numbers would be valid values for
+// num2 but not num1?
+
+//1 and 10 would be valid for num2 but not for num1**
+
+//
+
+// Exercise 7. Now we will try some addition.
+// a. Assign the the values of 4 and 6 to num1 and num2, respectively.
+num1 = 4;
+num2 = 6;
+// b.
+var theSum = num1 + num2;
+
+
+// Exercise 8. Now we will try some multiplication.
+// Make a new variable called 'theProduct', multiply num1 and num2
+var theProduct = num1 * num2;
+
+
+// Exercise 9.
+// Make a new variable called 'statement' and using 'myName' and
+var statement= "Hi, my name is " + myName ", and I can't wait to visit " + dreamDestination + "!";
+
+// *NOTE* For the following exercises, put your answers in the line below
+// the description of each exercise. For example, below a prompt, you may see:
+// let myAnswerForExercise10= "";
+// If your answer is "foo", then fill in the line to be:
+// let myAnswerForExercise10 = "foo";
+
+// Exercise 10. What is the data type of the value stored in 'dreamDestination',
+// place your answer in the quotes below:
+const myAnswerForExercise10 = "string";
+
+// Exercise 11. What is the data type of the value stored in 'product',
+// place your answer in the quotes below:
+
+const myAnswerForExercise11 = "integer";
+
+// Exercise 12. What do you think the value of emptyVariable is?
+// Place your answer below:
+
+const myAnswerForExercise12 = "null";
+
+// Exercise 13. If
+const A = "R";
+const B = 1;
+const C = 4;
+const D = "D";
+
+// What is the value of B + C
+const myAnswerForExercise13 = "5";
+
+// Exercise 14 What is the value of
+// A + "n" + D
+const myAnswerForExercise14 = "RnD";
+
+// Exercise 15 What is the value of
+// A + B * 2 + D + C / 2:
+const myAnswerForExercise15 = "R2D2";
+
+// Exercise 16 What is the value of A * B
+const myAnswerForExercise16 = "NaN";
+//not A number
+
+// Exercise 17 What is the value of true || false
+const myAnswerForExercise17 = "true";
+
+// Exercise 18 What is the value of 1 === "1"
+const myAnswerForExercise18 = "false";
+
+// Exercise 19 What is the value of 1 == "1"
+const myAnswerForExercise19 = "true";
+
+// Exercise 20 What is the value of 1 < 1
+const myAnswerForExercise20 = "false";
+
+// Exercise 21 What is the value of 1 < 2 < 3
+const myAnswerForExercise21 = "true";
+
+// Exercise 22 What is the value of 2 < 1 < 3
+const myAnswerForExercise22 = "true";
+//WHY IS THIS TRUE AHHHHHHH?????!!!!!!! AHHHHH!!!!!!!!
+
+
+//references:
+/*
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
+
+https://medium.com/infancyit/difference-between-var-let-and-const-in-es6-16a08d74b8b2
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
+
+https://stackoverflow.com/questions/762011/whats-the-difference-between-using-let-and-var
+
+https://stackoverflow.com/questions/11250449/can-you-add-a-condition-to-a-variable-declaration
+
+https://www.w3schools.com/jsref/jsref_number_nan.asp#:~:text=The%20NaN%20property%20represents%20%22Not,value%20is%20a%20NaN%20value.
+
+*/
